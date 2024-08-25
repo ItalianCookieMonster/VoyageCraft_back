dda2154acecc55983ef64147c5609711dacc43e5
Valentina Toni <vale.toni93@gmail.com>
2024-08-25 21:21:40 +0200

    DONE: CreateUserView and urls.py paths to create user and to tokens

--- users_app/serializer.py
+++ users_app/serializer.py
@@ -5,8 +5,12 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'age']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'age', 'password']
         extra_kwargs = {
             'id': {'read_only': True},
             'password': {'write_only': True}
         }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
--- voyage_craft/settings.py
+++ voyage_craft/settings.py
@@ -57,9 +57,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users_app'
+    'users_app',
     'rest_framework',
-    'rest_framework_simplejwt',
     'corsheaders',
 
 ]
