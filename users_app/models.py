00bc4053c402d8cc2c11143c4d97ab6d7f284266
Valentina Toni <vale.toni93@gmail.com>
2024-08-25 17:34:51 +0200

    DONE: Create CustomUser model and register it in Django admin

--- users_app/models.py
+++ users_app/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
