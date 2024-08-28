9cd8b3bcea4cef91ddd1ae75b37913f5ba982af4
Seda07 <gevorgyanseda0@gmail.com>
2024-08-28 15:46:43 +0200

    DONE migrete whit BBDD TODO test

--- itinerary/models.py
+++ itinerary/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
-from destinations.models import Destination
+from users_app.models import User
+from destinations.models import Destination, Activity
 class Itinerary(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField()
     start_date = models.DateField()
@@ -23,7 +23,7 @@ class ItineraryStep(models.Model):
     note = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    activity = models.ForeignKey('Activity', on_delete=models.CASCADE, null=True, blank=True)
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f"Step {self.step_order} of {self.itinerary.name}"
