e3ef472e92fd3595950b91dd961d200fb29cb602
Seda07 <gevorgyanseda0@gmail.com>
2024-08-28 15:16:29 +0200

    DONE views TODO test

--- itinerary/models.py
+++ itinerary/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from destinations.models import Destination
 class Itinerary(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField()
     start_date = models.DateField()
     end_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    destination = models.ForeignKey('Destination', on_delete=models.CASCADE)
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- itinerary/views.py
+++ itinerary/views.py
@@ -1,1 +1,16 @@
+from rest_framework import generics
+from .models import Itinerary
+from .serializer import ItinerarySerializer
+
+class CreateItineraryView(generics.CreateAPIView):
+    queryset = Itinerary.objects.all()
+    serializer_class = ItinerarySerializer
+
+class RetrieveUpdateDeleteItineraryView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Itinerary.objects.all()
+    serializer_class = ItinerarySerializer
+
+class ListItinerariesView(generics.ListAPIView):
+    queryset = Itinerary.objects.all()
+    serializer_class = ItinerarySerializer
 
